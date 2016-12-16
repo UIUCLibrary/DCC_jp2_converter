2db2e0aab6e88a44e56bf41afbaf837ae370bf5e
Henry Borchers <henryborchers@yahoo.com>
2016-12-16 16:48:43 -0600

    added find_access_folders, get_tiffs from file_manager up the namespace

--- dcc_jp2_converter/__init__.py
+++ dcc_jp2_converter/__init__.py
@@ -1,4 +1,10 @@
-from dcc_jp2_converter.exiv2Driver.exiv2commandbuilder import Exiv2CommandBuilder
-from dcc_jp2_converter.exiv2Driver import exic2CommandBuilders
-from dcc_jp2_converter.ImagemagickDriver.imagemagickcommandbuilder import ImagemagickCommandBuilder
-from dcc_jp2_converter.ImagemagickDriver import imagemagickCommandBuilders
+from .exiv2Driver.exiv2commandbuilder import Exiv2CommandBuilder
+from .exiv2Driver import exic2CommandBuilders
+from .ImagemagickDriver.imagemagickcommandbuilder import ImagemagickCommandBuilder
+from .ImagemagickDriver import imagemagickCommandBuilders
+from .file_manager import find_access_folders, get_tiffs
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
