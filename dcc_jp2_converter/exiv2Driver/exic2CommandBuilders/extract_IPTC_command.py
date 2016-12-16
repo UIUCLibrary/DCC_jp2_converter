abf933360d13f3f90623f7fd49387d9720a5aa42
Henry Borchers <henryborchers@yahoo.com>
2016-12-16 16:55:03 -0600

    updated documentation

--- dcc_jp2_converter/exiv2Driver/exic2CommandBuilders/extract_IPTC_command.py
+++ dcc_jp2_converter/exiv2Driver/exic2CommandBuilders/extract_IPTC_command.py
@@ -2,7 +2,13 @@ from .abs_builder import AbsBuilder
 
 
 class ExtractIPTCCommand(AbsBuilder):
-    """Extract IPTC dataset into a XMP sidecar file with extension .xmp"""
+    """Extract IPTC dataset into a XMP sidecar file with extension .xmp.
+
+    The equivalent exiv2 command might look like this::
+
+        exiv2 -eiX image.jpg
+
+    """
 
     def build_program_command(self):
         self._exiv_command.append(self.program_command)
