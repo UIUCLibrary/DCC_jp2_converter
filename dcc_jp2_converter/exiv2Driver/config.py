531a0a93e73421d7467d23947c0e00f463224cfe
Henry Borchers <henryborchers@yahoo.com>
2016-12-16 16:50:52 -0600

    added docstring

--- dcc_jp2_converter/exiv2Driver/config.py
+++ dcc_jp2_converter/exiv2Driver/config.py
@@ -3,6 +3,14 @@ import shutil
 
 
 def get_exiv2_path():
+    """
+    Attempts to retrieve the location to exiv2 command. If a path is specified in the settings ini file, that path will
+    be used. Otherwise, the function will search for it on the path.
+
+    Returns:
+        Full path to exiv2 command if found.
+
+    """
     config = configparser.ConfigParser()
     try:
         config.read("settings/settings.ini")
