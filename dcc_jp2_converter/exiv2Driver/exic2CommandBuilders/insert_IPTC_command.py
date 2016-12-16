eda9462e810e013171babe9a2eb1a553316582e3
Henry Borchers <henryborchers@yahoo.com>
2016-12-16 16:53:05 -0600

    added documentation

--- dcc_jp2_converter/exiv2Driver/exic2CommandBuilders/insert_IPTC_command.py
+++ dcc_jp2_converter/exiv2Driver/exic2CommandBuilders/insert_IPTC_command.py
@@ -2,7 +2,13 @@ from .abs_builder import AbsBuilder
 
 
 class InsertIPTCCommand(AbsBuilder):
-    """Inserts IPTC and XMP metadata from an XMP sidecar file into an image"""
+    """Inserts IPTC and XMP metadata from an XMP sidecar file into an image.
+
+    The equivalent exiv2 command might look like this::
+
+        exiv2 -iixX image.jpg
+
+    """
 
     def build_program_command(self):
         self._exiv_command.append(self.program_command)
