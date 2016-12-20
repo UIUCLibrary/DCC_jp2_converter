5aadddf85647ce68374aeaa45c0e758bc2c4b7aa
Henry Borchers <henryborchers@yahoo.com>
2016-12-20 16:46:45 -0600

    Added docstring

--- dcc_jp2_converter/__init__.py
+++ dcc_jp2_converter/__init__.py
@@ -1,6 +1,10 @@
-from .exiv2Driver.exiv2commandbuilder import Exiv2CommandBuilder
-from .exiv2Driver import exic2CommandBuilders
-from .ImagemagickDriver.imagemagickcommandbuilder import ImagemagickCommandBuilder
+"""
+This module contains all the logic for managing and excuting the task of
+converting jp2 tiff files into jp2 images for the digital library.
+"""
 from .ImagemagickDriver import imagemagickCommandBuilders
+from .ImagemagickDriver import imagemagickcommandbuilder
+from .exiv2Driver import exiv2CommandBuilders
+from .exiv2Driver.exiv2commandbuilder import Exiv2CommandBuilder
 from .file_manager import find_access_folders, get_tiffs
 from .utils import get_config_file
