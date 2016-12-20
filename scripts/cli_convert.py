e9bcb2e0a1014aa2d539386df9491698d33cd1ad
Henry Borchers <henryborchers@yahoo.com>
2016-12-20 16:48:02 -0600

    Added docstrings

--- scripts/cli_convert.py
+++ scripts/cli_convert.py
@@ -1,10 +1,12 @@
+"""Command line interface for DCC jp2 Converter."""
+
 import logging
 import os
 import sys
 
-import dcc_jp2_converter
 import argparse
 
+import dcc_jp2_converter
 from dcc_jp2_converter import converter
 from dcc_jp2_converter.logging import InfoFilter
 
@@ -12,20 +14,48 @@ from dcc_jp2_converter.logging import InfoFilter
 DEFAULT_LOG_FILE = "processing.log"
 logger = logging.getLogger(__package__)
 
-def get_args():
-    parser = argparse.ArgumentParser(description="Create JP2 files from tiffs for digital Library",
-                                     epilog="Settings for this script can be configured at {}: ".format(
-                                         dcc_jp2_converter.get_config_file()))
-    parser.add_argument('path', help="Path to the submission package")
-    parser.add_argument('--overwrite', action="store_true", help="Overwrite any existing jp2 with new ones")
-    parser.add_argument('--logname', default=DEFAULT_LOG_FILE, help="Change the log name.")
 
-    parser.add_argument('--debug', action="store_true", help="Run script in debug mode")
+def get_args():
+    """
+    Parses the command line arguments.
+
+    Returns:
+        Parsed command line arguments.
+
+    """
+    parser = argparse.ArgumentParser(
+        description="Create JP2 files from tiffs for digital Library",
+        epilog="Settings for this script can be configured at {}: ".format(
+            dcc_jp2_converter.get_config_file()))
+
+    parser.add_argument('path', help="Path to the submission package")
+
+    parser.add_argument(
+        '--overwrite',
+        action="store_true",
+        help="Overwrite any existing jp2 with new ones")
+
+    parser.add_argument(
+        '--logname',
+        default=DEFAULT_LOG_FILE,
+        help="Change the log name.")
+
+    parser.add_argument(
+        '--debug',
+        action="store_true",
+        help="Run script in debug mode")
+
     args = parser.parse_args()
+
     return args
 
 
 def find_arg_errors(args):
+    """
+    Perform error check on command line arguments passed in in args to see if
+    they are logically valid and return any errors if any are found.
+    """
+
     errors = []
     if not os.path.exists(args.path):
         errors.append("Unable to find directory, \"{}\"".format(args.path))
@@ -34,12 +64,21 @@ def find_arg_errors(args):
     return errors
 
 
-def configure_logger(logger, debug_mode=False, log_file=None):
+def configure_logger(debug_mode=False, log_file=None):
+    """
+    Configure the settings for the logger.
 
-    # logger.propagate = True
+    Args:
+        debug_mode: Set the package into debug more.
+        log_file: If a file name is given, all information presented to the
+         screen with be saved into that file.
+
+    """
+
     logger.setLevel(logging.DEBUG)
 
-    logging_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging_formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     std_handler = logging.StreamHandler(sys.stdout)
     std_handler.setLevel(logging.INFO)
@@ -61,7 +100,6 @@ def configure_logger(logger, debug_mode=False, log_file=None):
 
     logger.addHandler(std_handler)
     logger.addHandler(err_handler)
-    return logger
 
 
 def main():
@@ -71,8 +109,7 @@ def main():
         print("Using DEBUG mode!")
 
     # TODO: Make logging optional
-    configure_logger(logger, debug_mode=args.debug, log_file=args.logname)
-    # logger = dcc_jp2_converter.get_logger()
+    configure_logger(debug_mode=args.debug, log_file=args.logname)
     logger.debug("Validating command line arguments.")
     errors = find_arg_errors(args)
     if errors:
@@ -82,21 +119,23 @@ def main():
         exit(1)
 
     logger.debug("Command line arguments are valid.")
-    logger.info("Searching \"{}\" for object folders with access tiffs".format(args.path))
+    logger.info(
+        "Searching \"{}\" for object folders with access tiffs".format(
+            args.path))
 
     folders = list(dcc_jp2_converter.find_access_folders(args.path))
     try:
         for i, folder in enumerate(folders):
-            logger.info("Item: {} of {}: \"{}\"".format(i + 1, len(folders), folder))
-            converter.convert_tiff_access_folder(folder, overwrite_existing=args.overwrite)
+            logger.info(
+                "Item: {} of {}: \"{}\"".format(i + 1, len(folders), folder))
+            converter.convert_tiff_access_folder(
+                folder, overwrite_existing=args.overwrite)
 
         print("\n\nAll Done!\n")
     except KeyboardInterrupt:
-        print("Job Terminated")
+        logger.warning("Job Terminated")
     print("Log file located at {}".format(os.path.abspath(DEFAULT_LOG_FILE)))
 
 
 if __name__ == '__main__':
     main()
-
-
