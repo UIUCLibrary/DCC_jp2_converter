9e4a0cbfdea97aad4bc61118f57f088c494337d2
Henry Borchers <henryborchers@yahoo.com>
2016-12-20 14:15:56 -0600

    removed logging

--- dcc_jp2_converter/__init__.py
+++ dcc_jp2_converter/__init__.py
@@ -3,5 +3,4 @@ from .exiv2Driver import exic2CommandBuilders
 from .ImagemagickDriver.imagemagickcommandbuilder import ImagemagickCommandBuilder
 from .ImagemagickDriver import imagemagickCommandBuilders
 from .file_manager import find_access_folders, get_tiffs
-from .logging import configure_logger, get_logger
 from .utils import get_config_file
