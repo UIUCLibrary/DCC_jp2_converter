c494c898756a6a97611884e0ff285f6e3a2000c1
Henry Borchers <henryborchers@yahoo.com>
2016-12-20 14:17:55 -0600

    updated logging to it propagate correctly.

--- scripts/cli_convert.py
+++ scripts/cli_convert.py
@@ -1,15 +1,25 @@
+import logging
 import os
+import sys
 
 import dcc_jp2_converter
 import argparse
-from dcc_jp2_converter import utils
-from dcc_jp2_converter import logger
-from dcc_jp2_converter import converter
 
+from dcc_jp2_converter import converter
+from dcc_jp2_converter.logging import InfoFilter
+
+
+DEFAULT_LOG_FILE = "processing.log"
+logger = logging.getLogger(__package__)
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Create JP2 files from tiffs for digital Library")
+    parser = argparse.ArgumentParser(description="Create JP2 files from tiffs for digital Library",
+                                     epilog="Settings for this script can be configured at {}: ".format(
+                                         dcc_jp2_converter.get_config_file()))
     parser.add_argument('path', help="Path to the submission package")
+    parser.add_argument('--overwrite', action="store_true", help="Overwrite any existing jp2 with new ones")
+    parser.add_argument('--logname', default=DEFAULT_LOG_FILE, help="Change the log name.")
+
     parser.add_argument('--debug', action="store_true", help="Run script in debug mode")
     args = parser.parse_args()
     return args
@@ -24,14 +34,45 @@ def find_arg_errors(args):
     return errors
 
 
+def configure_logger(logger, debug_mode=False, log_file=None):
+
+    # logger.propagate = True
+    logger.setLevel(logging.DEBUG)
+
+    logging_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    std_handler = logging.StreamHandler(sys.stdout)
+    std_handler.setLevel(logging.INFO)
+    std_handler.addFilter(InfoFilter())
+
+    err_handler = logging.StreamHandler()
+    err_handler.setLevel(logging.WARNING)
+
+    if log_file:
+        fl_handler = logging.FileHandler(log_file)
+        fl_handler.setLevel(logging.INFO)
+        fl_handler.setFormatter(logging_formatter)
+        logger.addHandler(fl_handler)
+    if debug_mode:
+        std_handler.setLevel(logging.DEBUG)
+
+        std_handler.setFormatter(logging_formatter)
+        err_handler.setFormatter(logging_formatter)
+
+    logger.addHandler(std_handler)
+    logger.addHandler(err_handler)
+    return logger
+
+
 def main():
+
     args = get_args()
     if args.debug:
         print("Using DEBUG mode!")
 
     # TODO: Make logging optional
-    utils.configure_logger(debug_mode=args.debug, log_file="/Users/hborcher/PycharmProjects/DCC_jp2_converter/mylog.log")
-
+    configure_logger(logger, debug_mode=args.debug, log_file=args.logname)
+    # logger = dcc_jp2_converter.get_logger()
     logger.debug("Validating command line arguments.")
     errors = find_arg_errors(args)
     if errors:
@@ -44,16 +85,18 @@ def main():
     logger.info("Searching \"{}\" for object folders with access tiffs".format(args.path))
 
     folders = list(dcc_jp2_converter.find_access_folders(args.path))
-    for i, folder in enumerate(folders):
-        logger.info("{} of {}: \"{}\"".format(i+1, len(folders), folder))
-        converter.convert_tiff_access_folder(folder)
+    try:
+        for i, folder in enumerate(folders):
+            logger.info("Item: {} of {}: \"{}\"".format(i + 1, len(folders), folder))
+            converter.convert_tiff_access_folder(folder, overwrite_existing=args.overwrite)
 
-
-
-
-
+        print("\n\nAll Done!\n")
+    except KeyboardInterrupt:
+        print("Job Terminated")
+    print("Log file located at {}".format(os.path.abspath(DEFAULT_LOG_FILE)))
 
 
 if __name__ == '__main__':
-
     main()
+
+
