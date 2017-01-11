250088b5ec85c0a495637ef805fc9689c8906f74
Henry Borchers <henryborchers@yahoo.com>
2017-01-11 16:18:29 -0600

    use multiple config files if they exists

--- dcc_jp2_converter/utils.py
+++ dcc_jp2_converter/utils.py
@@ -1,6 +1,9 @@
 from pkg_resources import resource_filename, Requirement
+from pathlib import Path
 
-
-def get_config_file():
-    return resource_filename(
-        Requirement.parse("DCC_jp2_converter"), "settings/settings.ini")
+def get_config_files():
+    config_files = [
+        resource_filename(Requirement.parse("DCC_jp2_converter"), "settings/command_paths.ini"),
+        str(Path.home() / "command_paths.ini")
+    ]
+    return config_files
