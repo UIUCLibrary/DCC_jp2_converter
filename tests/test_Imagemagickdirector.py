07810cd82118803e6029066bbd7fb0fa1c970d55
Henry Borchers <henryborchers@yahoo.com>
2017-01-11 16:17:56 -0600

    added a test_ignore_exif_builder_tiff2jp2 test

--- tests/test_Imagemagickdirector.py
+++ tests/test_Imagemagickdirector.py
@@ -1,6 +1,7 @@
 import os
 
 from dcc_jp2_converter import ImagemagickDriver as driver
+from dcc_jp2_converter import imagemagickCommandBuilders as builders
 
 import pytest
 
@@ -42,3 +43,26 @@ def test_default_builder_tiff2jp2(default_builder):
     assert command == ['/usr/bin/convert',
                        '/Users/Documents/sample.tif',
                        '/Users/Documents/output.jp2']
+
+
+def test_ignore_exif_builder_tiff2jp2(monkeypatch):
+    """
+    Test that the IgnoreExif builder works
+
+    """
+    IMAGEMAGICK = "/usr/bin/convert"
+    src_file = "/Users/Documents/sample.tif"
+    dst_file = "/Users/Documents/output.jp2"
+    expected_cmd = ['/usr/bin/convert',
+                    '-define', 'tiff:exif-properties=false',
+                    '/Users/Documents/sample.tif',
+                    '/Users/Documents/output.jp2']
+    # Because the location of imagemagick is faked for this example,
+    # we need to ignore the checks
+    monkeypatch.setattr(os.path, "exists", lambda x: True)
+
+    command_builder = driver.ImagemagickCommandBuilder(builder=builders.IgnoreExif(),
+                                                       program_path=IMAGEMAGICK)
+
+    command = command_builder.build_command(src=src_file, dst=dst_file)
+    assert command == expected_cmd
