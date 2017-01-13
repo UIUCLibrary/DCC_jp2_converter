fa2450140706c6c9db9a045787293fed4c359c72
hborcher <hborcher@illinois.edu>
2017-01-13 15:04:19 -0600

    Creates a settings report if something fails.

--- scripts/cli_convert.py
+++ scripts/cli_convert.py
@@ -1,20 +1,29 @@
 """Command line interface for DCC jp2 Converter."""
-
+import argparse
+import configparser
 import logging
 import os
 import sys
-
-import argparse
+import traceback
 
 import dcc_jp2_converter
 from dcc_jp2_converter import converter
-from dcc_jp2_converter.ImagemagickDriver import get_imagemagick_path
-from dcc_jp2_converter.exiv2Driver import get_exiv2_path
 from dcc_jp2_converter.logging import InfoFilter
+from scripts.validation import find_settings_errors, find_arg_errors
 
-
+ERROR_LOGGING_FILE = "errors.log"
 DEFAULT_LOG_FILE = "processing.log"
 logger = logging.getLogger(__package__)
+
+
+def get_logging_name(config_files):
+    config = configparser.ConfigParser()
+    try:
+        for config_file in config_files:
+            config.read(config_file)
+        return config['logging'].get('logname', DEFAULT_LOG_FILE)
+    except KeyError:
+        return DEFAULT_LOG_FILE
 
 
 def get_args():
@@ -25,10 +34,16 @@ def get_args():
         Parsed command line arguments.
 
     """
+    try:
+        config_files = dcc_jp2_converter.get_config_files()
+    except FileNotFoundError:
+        print("No command_paths.ini config file found.", file=sys.stderr)
+        raise
+
     parser = argparse.ArgumentParser(
         description="Create JP2 files from tiffs for digital Library",
         epilog="Settings for this script can be configured at {}: ".format(
-            dcc_jp2_converter.get_config_files()[-1]))
+            config_files[-1]))
 
     parser.add_argument('path', help="Path to the submission package")
 
@@ -39,7 +54,7 @@ def get_args():
 
     parser.add_argument(
         '--logname',
-        default=DEFAULT_LOG_FILE,
+        default=get_logging_name(config_files),
         help="Change the log name.")
 
     parser.add_argument(
@@ -50,81 +65,6 @@ def get_args():
     args = parser.parse_args()
 
     return args
-
-
-def is_executable(path):
-    if sys.platform == "win32":
-        if os.path.splitext(path)[1].lower() == ".exe":
-            return True
-        else:
-            return False
-
-    # Unix based
-    else:
-        return os.access(path, os.X_OK)
-
-
-def find_settings_errors():
-    """
-    Looks for any configuration errors to make sure that the script can operate properly
-    with the given settings by the default path lookup or any changes provided by a
-    command_paths.ini file.
-
-    Returns:
-        Returns a list of error messages if any. Returns an empty list if found no errors.
-
-    """
-    errors = []
-    missing_programs = []
-
-    not_found_message = "The following required programs were not found: \"{}\". Please make sure they installed " \
-                        "on the path or command_paths.ini file is configured to point to their location in your home " \
-                        "folder."
-
-    ########################
-    # Imagemagick settings #
-    ########################
-    imagemagick_path = get_imagemagick_path()
-
-    # check exists
-    if imagemagick_path is None or not os.path.exists(imagemagick_path):
-        missing_programs.append("Imagemagick's convert command")
-    else:
-        # Check if found Windows "convert FAL to NTFS command" by mistake and if the item is executable
-        if "system32" in imagemagick_path.lower() or not is_executable(imagemagick_path):
-            errors.append("Imagemagick's convert command, \"{}\",is invalid. Check command_paths.ini file.".format(imagemagick_path))
-
-    ##################
-    # exiv2 settings #
-    ##################
-    exiv2_path = get_exiv2_path()
-
-    # check exists
-    if exiv2_path is None or not os.path.exists(exiv2_path):
-        missing_programs.append("exiv2")
-    else:
-
-        # check is executable
-        if not is_executable(exiv2_path):
-            errors.append("exiv2 command, \"{}\", is invalid. Check command_paths.ini file.".format(exiv2_path))
-    if missing_programs:
-        errors.append(not_found_message.format("\", \"".join(missing_programs)))
-
-    return errors
-
-
-def find_arg_errors(args):
-    """
-    Perform error check on command line arguments passed in in args to see if
-    they are logically valid and return any errors if any are found.
-    """
-
-    errors = []
-    if not os.path.exists(args.path):
-        errors.append("Unable to find directory, \"{}\"".format(args.path))
-    if os.path.isfile(args.path):
-        errors.append("\"{}\" is not a directory.".format(args.path))
-    return errors
 
 
 def configure_logger(debug_mode=False, log_file=None):
@@ -165,21 +105,26 @@ def configure_logger(debug_mode=False, log_file=None):
     logger.addHandler(err_handler)
 
 
-def main():
+def run():
 
+    # Get user args
     args = get_args()
+
     if args.debug:
         print("Using DEBUG mode!")
 
-    # TODO: Make logging optional
+    # Set up logging
     configure_logger(debug_mode=args.debug, log_file=args.logname)
     logger.debug("Validating command line arguments.")
+
+    # Do error checking
     errors = find_arg_errors(args)
     if errors:
         for error in errors:
             logger.error(error)
-        logger.error("Script terminated due to invalid arguments.")
-        exit(1)
+        error_msg = "Invalid arguments."
+        logger.error(error_msg)
+        raise ValueError("{}\n{}".format(error_msg, "\n".join(errors)))
 
     logger.debug("Command line arguments are valid.")
     logger.info(
@@ -190,22 +135,54 @@ def main():
     if errors:
         for error in errors:
             logger.error(error)
-        logger.error("Script terminated due to invalid settings.")
-        exit(1)
+        error_msg = "Invalid settings."
+        logger.error(error_msg)
+        raise ValueError("{}\n{}".format(error_msg, "\n".join(errors)))
 
+    # Find all the folders that contain files that need to be converted
     folders = list(dcc_jp2_converter.find_access_folders(args.path))
+
     try:
         for i, folder in enumerate(folders):
             logger.info(
                 "Item: {} of {}: \"{}\"".format(i + 1, len(folders), folder))
+
+            # Do the work!
             converter.convert_tiff_access_folder(
                 folder, overwrite_existing=args.overwrite)
 
         print("\n\nAll Done!\n")
     except KeyboardInterrupt:
         logger.warning("Job Terminated")
-    print("Log file located at {}".format(os.path.abspath(DEFAULT_LOG_FILE)))
+    print("Log file located at {}".format(os.path.abspath(args.logname)))
 
+
+def main():
+    try:
+        run()
+
+    except KeyboardInterrupt:
+        print("Exiting")
+
+    except Exception as e:
+        print("\n==============================", file=sys.stderr)
+        print("Script failed due to an error.", file=sys.stderr)
+        tb = traceback.format_exc()
+        print("Reason: {}".format(e), file=sys.stderr)
+
+        try:
+            with open(ERROR_LOGGING_FILE, "w", encoding="utf8") as f:
+                f.write("Stacktrace:\n")
+                f.write(tb)
+                args = str(get_args())
+                f.write("\nArgs:\n")
+                f.write(args)
+
+                print("Detailed info about this error was saved to \"{}\"".format(ERROR_LOGGING_FILE), file=sys.stderr)
+        finally:
+            print("Script terminated.", file=sys.stderr)
+
+        exit(1)
 
 if __name__ == '__main__':
     main()
