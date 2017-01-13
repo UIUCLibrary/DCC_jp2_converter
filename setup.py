7dcfd1603a8a347895444301c3598cdaa485914f
hborcher <hborcher@illinois.edu>
2017-01-13 13:54:28 -0600

    version is now taken from dcc_jp2_converter's __version__

--- setup.py
+++ setup.py
@@ -4,27 +4,29 @@ Script to create JP2 access images with embedded metadata from tiff access
 masters to be used by the Digital Library.
 
 """
-
+import os
 from setuptools import setup
+from dcc_jp2_converter import __version__ as version
 
 setup(
     name='DCC_jp2_converter',
-    version='0.0.1b1',
+    version=version,
     packages=['dcc_jp2_converter',
               'dcc_jp2_converter.exiv2Driver',
               'dcc_jp2_converter.exiv2Driver.exiv2CommandBuilders',
               'dcc_jp2_converter.ImagemagickDriver',
               'dcc_jp2_converter.ImagemagickDriver.imagemagickCommandBuilders',
+              'scripts'
               ],
-    scripts=['scripts/cli_convert.py'],
     entry_points={
-        'console_scripts': ['makejp2=cli_convert:main']
+        'console_scripts': ['makejp2=scripts.cli_convert:run']
     },
     test_suite="tests",
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     data_files=[
         ('settings', ['settings/command_paths.ini']),
+        (os.path.expanduser("~"), ['settings/command_paths.ini']),
     ],
     zip_safe=False,
     url='https://github.com/UIUCLibrary/DCC_jp2_converter',
