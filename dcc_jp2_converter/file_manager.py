88273a2a8700d7b611c6076366b67d9c8e3d0d2e
Henry Borchers <henryborchers@yahoo.com>
2017-01-19 13:14:21 -0600

    find_converted_pair

--- dcc_jp2_converter/file_manager.py
+++ dcc_jp2_converter/file_manager.py
@@ -1,5 +1,9 @@
+import typing
+from collections import namedtuple
 from typing import Iterator
 import os
+
+CollectionPair = namedtuple("CollectionPair", ['tiff', 'jp2'])
 
 
 def find_access_folders(path) -> Iterator[str]:
@@ -34,3 +38,23 @@ def get_tiffs(path) -> Iterator[str]:
         if os.path.splitext(item.name)[1].lower() != ".tif":
             continue
         yield item.path
+
+
+def find_converted_pair(folder: str) -> typing.Iterator[CollectionPair]:
+    """
+    Search recursively from a path to find matching pairs of jp2 and tiff files with the same basename in the same
+    folder.
+
+
+    Args:
+        folder: root directory to search from.
+
+    Yields: Matches, if any.
+
+    """
+    for root, dirs, files in os.walk(folder):
+        for file_ in files:
+            if os.path.splitext(file_)[1].lower() == ".jp2":
+                possible_tiff_match = os.path.splitext(file_)[0] + ".tif"
+                if possible_tiff_match in files:
+                    yield CollectionPair(tiff=os.path.join(root, possible_tiff_match), jp2=os.path.join(root, file_))
