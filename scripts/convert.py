c3dd4baec7b67536a74a3dc50c43d2b29e43a52a
Henry Borchers <henryborchers@yahoo.com>
2017-01-19 14:53:27 -0600

    init commit

---
+++ scripts/convert.py
@@ -0,0 +1,23 @@
+import dcc_jp2_converter
+from dcc_jp2_converter import converter
+import logging
+
+logger = logging.getLogger(__package__)
+
+
+def convert_path(args):
+    logger.info(
+        "Searching \"{}\" for object folders with access tiffs.".format(
+            args.path))
+
+    folders = list(dcc_jp2_converter.find_access_folders(args.path))
+    for i, folder in enumerate(folders):
+        logger.info(
+            "Folder: {} of {}: \"{}\"".format(i + 1, len(folders), folder))
+
+        # Do the work!
+        converter.convert_tiff_access_folder(
+            folder,
+            overwrite_existing=args.overwrite,
+            remove_on_success=args.remove
+        )
