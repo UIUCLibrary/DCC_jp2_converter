8b450a09f4fdeee4fe4e58788a42ad5385d81717
hborcher <hborcher@illinois.edu>
2017-01-24 10:25:03 -0600

    added Standard

--- dcc_jp2_converter/modules/ImagemagickDriver/imagemagickCommandBuilders/__init__.py
+++ dcc_jp2_converter/modules/ImagemagickDriver/imagemagickCommandBuilders/__init__.py
@@ -1,10 +1,12 @@
 from .default_command_builder import DefaultCommandBuilder
 from .jp2_access_command_builder import Jp2AccessCommandBuilder
 from .IgnoreExif_command_builder import IgnoreExif
+from .standard_command_builder import Standard
 
 __all__ = [
     'DefaultCommandBuilder',
     'Jp2AccessCommandBuilder',
     'IgnoreExif',
+    'Standard'
     'abs_builder',
 ]
