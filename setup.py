8adcf7f125b147923cefa77f7305cf8fef230122
hborcher <hborcher@illinois.edu>
2017-04-13 17:10:05 -0500

    refactored shared settings to common file

--- setup.py
+++ setup.py
@@ -6,40 +6,23 @@ masters to be used by the Digital Library.
 """
 import os
 from setuptools import setup
+from shared_setup import metadata
 
-from dcc_jp2_converter import __version__ as version
+setup(**metadata,
+      entry_points={
+          'console_scripts': ['makejp2=dcc_jp2_converter.scripts.cli:main']
+      },
+      package_data={
+          'dcc_jp2_converter.thirdparty': ['*'],
+      },
+      include_package_data=True,
+      test_suite="tests",
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      data_files=[
+          ('settings', ['settings/command_paths.ini']),
+          (os.path.expanduser("~"), ['settings/command_paths.ini']),
+      ],
+      zip_safe=False,
 
-setup(
-    name='DCC_jp2_converter',
-    version=version,
-    packages=['dcc_jp2_converter',
-              'dcc_jp2_converter.modules',
-              'dcc_jp2_converter.modules.exiv2Driver',
-              'dcc_jp2_converter.modules.exiv2Driver.exiv2CommandBuilders',
-              'dcc_jp2_converter.modules.ImagemagickDriver',
-              'dcc_jp2_converter.modules.ImagemagickDriver.imagemagickCommandBuilders',
-              'dcc_jp2_converter.modules.kakaduDriver',
-              'dcc_jp2_converter.scripts',
-              'dcc_jp2_converter.thirdparty'
-              ],
-    entry_points={
-        'console_scripts': ['makejp2=dcc_jp2_converter.scripts.cli:main']
-    },
-    package_data={
-      'dcc_jp2_converter.thirdparty': ['*'],
-    },
-    include_package_data=True,
-    test_suite="tests",
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    data_files=[
-        ('settings', ['settings/command_paths.ini']),
-        (os.path.expanduser("~"), ['settings/command_paths.ini']),
-    ],
-    zip_safe=False,
-    url='https://github.com/UIUCLibrary/DCC_jp2_converter',
-    license='',
-    author='Henry Borchers',
-    author_email='hborcher@illinois.edu',
-    description='DCC tool for building JP2 access files from Tiff files'
-)
+      )
