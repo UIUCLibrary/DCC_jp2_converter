2e9b997ba9c068952b220bf86c6140860d7a512a
Henry Borchers <henryborchers@yahoo.com>
2017-04-13 15:37:49 -0500

    get_kdu_compress_path first checks for a entry in the config settings. Then checks if it's bundled in the thirdparty package. finally if it can't find it, it checks on the path before failing

--- dcc_jp2_converter/modules/kakaduDriver/config.py
+++ dcc_jp2_converter/modules/kakaduDriver/config.py
@@ -1,7 +1,9 @@
 import configparser
+import os
 import shutil
 
 from dcc_jp2_converter.modules.utils import get_config_files
+import dcc_jp2_converter.thirdparty
 
 
 def get_kdu_compress_path():
@@ -14,10 +16,45 @@ def get_kdu_compress_path():
         Full path to exiv2 command if found.
 
     """
-    config = configparser.ConfigParser()
+
+    def check_config():
+        config = configparser.ConfigParser()
+        try:
+            for config_file in get_config_files():
+                config.read(config_file)
+            command = config['commands']['kdu_compress']
+            if not os.path.exists(command):
+                raise FileNotFoundError
+            return command
+        except KeyError:
+            raise FileNotFoundError
+
+    def check_bundled():
+
+        path = shutil.which("kdu_compress", path=dcc_jp2_converter.thirdparty.__path__[0])
+        if path:
+            return path
+        else:
+            raise FileNotFoundError
+
+    def check_path():
+        path = shutil.which("kdu_compress")
+        if path:
+            return path
+        else:
+            raise FileNotFoundError
+
+    # Check the location in the config file setting
     try:
-        for config_file in get_config_files():
-            config.read(config_file)
-        return config['commands'].get('kdu_compress', shutil.which("kdu_compress"))
-    except KeyError:
-        return shutil.which("kdu_compress")
+        return check_config()
+    except FileNotFoundError:
+        pass
+
+    # Check the for a config file setting
+    try:
+        return check_bundled()
+    except FileNotFoundError:
+        pass
+
+    # If all else fails, check the path
+    return check_path()
