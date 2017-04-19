19e0d4d65b2cc1087871af4c45bf169c82189a95
hborcher <hborcher@illinois.edu>
2017-04-19 16:11:16 -0500

    brought the shared metadata together because it was having a problem with TOX

--- setup_freeze.py
+++ setup_freeze.py
@@ -2,17 +2,33 @@ import platform
 import os
 import dcc_jp2_converter.thirdparty
 from cx_Freeze import setup, Executable
-from shared_setup import metadata
+from dcc_jp2_converter import __version__ as version
 
+metadata = {"name": 'DCC_jp2_converter',
+            "version": version,
+            "packages": ['dcc_jp2_converter',
+                         'dcc_jp2_converter.modules',
+                         'dcc_jp2_converter.modules.exiv2Driver',
+                         'dcc_jp2_converter.modules.exiv2Driver.exiv2CommandBuilders',
+                         'dcc_jp2_converter.modules.ImagemagickDriver',
+                         'dcc_jp2_converter.modules.ImagemagickDriver.imagemagickCommandBuilders',
+                         'dcc_jp2_converter.modules.kakaduDriver',
+                         'dcc_jp2_converter.scripts',
+                         'dcc_jp2_converter.thirdparty'
+                         ],
+            "url": 'https://github.com/UIUCLibrary/DCC_jp2_converter', "license": '', "author": 'Henry Borchers',
+            "author_email": 'hborcher@illinois.edu',
+            "description": 'DCC tool for building JP2 access files from Tiff files', "options": {
+        "build_exe": {
+            "includes": ["queue", "atexit"],
+            "packages": ["os"],
+            "excludes": ["tkinter"],
+        }
+    }, 'executables': [Executable("dcc_jp2_converter/scripts/cli.py",
+                                  targetName=("makejp2.exe" if platform.system() == "Windows" else "makejp2"))]}
 
-metadata['options'] = {
-    "build_exe": {
-        "includes": ["queue", "atexit"],
-        "packages": ["os"],
-        "excludes": ["tkinter"],
-    }
-}
-metadata['executables'] = [Executable("dcc_jp2_converter/scripts/cli.py",
-                                      targetName=("makejp2.exe" if platform.system() == "Windows" else "makejp2"))]
+# metadata['options'] = {
+#
+# }
 
 setup(**metadata)
