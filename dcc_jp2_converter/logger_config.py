f70e431d2e9e2c3bcc5c8066cc9cbcc7759c1f75
hborcher <hborcher@illinois.edu>
2017-05-26 13:25:57 -0500

    init commit refactored from cli

---
+++ dcc_jp2_converter/logger_config.py
@@ -0,0 +1,48 @@
+import logging
+import sys
+
+from dcc_jp2_converter.scripts import logging_settings
+
+
+def configure_logger(debug_mode=False, log_file=None):
+    logger = logging.getLogger(__package__)
+
+    """
+    Configure the settings for the logger.
+
+    Args:
+        debug_mode: Set the package into debug mode.
+        log_file: If a file name is given, all information presented to the
+         screen with be saved into that file.
+
+    """
+
+    logger.setLevel(logging.DEBUG)
+
+    logging_formatter_friendly = logging.Formatter('%(asctime)s: %(message)s', datefmt='%I:%M:%S %p')
+    logging_formatter_verbose = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    std_handler = logging.StreamHandler(sys.stdout)
+    std_handler.setLevel(logging.INFO)
+    std_handler.addFilter(logging_settings.InfoFilter())
+
+    err_handler = logging.StreamHandler()
+    err_handler.setLevel(logging.WARNING)
+
+    if log_file:
+        fl_handler = logging.FileHandler(log_file)
+        fl_handler.setLevel(logging.INFO)
+        fl_handler.setFormatter(logging_formatter_verbose)
+        logger.addHandler(fl_handler)
+    if debug_mode:
+        std_handler.setLevel(logging.DEBUG)
+
+        std_handler.setFormatter(logging_formatter_verbose)
+        err_handler.setFormatter(logging_formatter_verbose)
+    else:
+        err_handler.setFormatter(logging_formatter_friendly)
+        std_handler.setFormatter(logging_formatter_friendly)
+
+    logger.addHandler(std_handler)
+    logger.addHandler(err_handler)
