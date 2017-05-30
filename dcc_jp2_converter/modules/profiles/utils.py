1aa570dffdadd50812ea9cbbc35666bcdc7ccf8b
hborcher <hborcher@illinois.edu>
2017-05-30 11:07:24 -0500

    updated get_profile to make sure use case insensitive for looking up the profile

--- dcc_jp2_converter/modules/profiles/utils.py
+++ dcc_jp2_converter/modules/profiles/utils.py
@@ -3,7 +3,7 @@ from dcc_jp2_converter.modules.profiles import profile, profile_factory
 
 def get_profile(factory_name) -> profile.AbsProfile:
     factory = profile_factory.ProfileFactory()
-    new_profile = factory.create_instance(factory_name)
+    new_profile = factory.create_instance(factory_name.lower())
     new_profile.configure()
     return new_profile
 
