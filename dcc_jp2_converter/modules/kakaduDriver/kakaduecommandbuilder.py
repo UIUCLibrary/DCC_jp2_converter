cd236c24078ab75d86b114e7e35dfa892581ef1c
hborcher <hborcher@illinois.edu>
2017-05-30 15:12:52 -0500

    replaced call to get_kdu_compress_path to KduCompressPath class

--- dcc_jp2_converter/modules/kakaduDriver/kakaduecommandbuilder.py
+++ dcc_jp2_converter/modules/kakaduDriver/kakaduecommandbuilder.py
@@ -5,14 +5,21 @@ from dcc_jp2_converter.modules.kakaduDriver import config
 
 class KakaduCommandBuilder:
     def __init__(self, builder, program_path=None):
-        if not program_path:
-            program_path = config.get_kdu_compress_path()
-        self.program_path = program_path
+        if program_path:
+            self.program_path = program_path
+        else:
+            path_finder = config.KduCompressPath()
+            try:
+                self.program_path = path_finder.get_path()
+            except FileNotFoundError:
+                raise FileNotFoundError("kakdu compress not found")
+            # program_path = config.get_kdu_compress_path()
+        # self.program_path = program_path
         self._builder = builder
 
-        if not os.path.exists(program_path):
-            raise FileNotFoundError(
-                "Unable to find {} program.".format(program_path))
+        # if not os.path.exists(program_path):
+        #     raise FileNotFoundError(
+        #         "Unable to find {} program.".format(program_path))
 
     def build_command(self, src: str, dst: str) -> list:
         self._builder.new_command()
