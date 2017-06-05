356a638909e94ab386ef0f7949fe88bfc8cb919f
hborcher <hborcher@illinois.edu>
2017-06-05 10:46:11 -0500

    added cx-freeze

--- cx_setup.py
+++ cx_setup.py
@@ -3,17 +3,20 @@ import os
 import dcc_jp2_converter.thirdparty
 from cx_Freeze import setup, Executable
 import dcc_jp2_converter
+
 metadata = {
-            "packages": ['dcc_jp2_converter',
-                         'dcc_jp2_converter.modules',
-                         'dcc_jp2_converter.modules.exiv2Driver',
-                         'dcc_jp2_converter.modules.exiv2Driver.exiv2CommandBuilders',
-                         'dcc_jp2_converter.modules.ImagemagickDriver',
-                         'dcc_jp2_converter.modules.ImagemagickDriver.imagemagickCommandBuilders',
-                         'dcc_jp2_converter.modules.kakaduDriver',
-                         'dcc_jp2_converter.scripts',
-                         'dcc_jp2_converter.thirdparty'
-                         ],
+    "packages": [
+        'settings',
+        'dcc_jp2_converter',
+        'dcc_jp2_converter.modules',
+        'dcc_jp2_converter.modules.exiv2Driver',
+        'dcc_jp2_converter.modules.exiv2Driver.exiv2CommandBuilders',
+        'dcc_jp2_converter.modules.ImagemagickDriver',
+        'dcc_jp2_converter.modules.ImagemagickDriver.imagemagickCommandBuilders',
+        'dcc_jp2_converter.modules.kakaduDriver',
+        'dcc_jp2_converter.scripts',
+        'dcc_jp2_converter.thirdparty'
+    ],
     "options": {
         "build_exe": {
             "includes": ["queue", "atexit", "six", "pyparsing", "appdirs"],
@@ -24,7 +27,6 @@ metadata = {
     }, 'executables': [Executable("dcc_jp2_converter/scripts/cli.py",
                                   targetName=("makejp2.exe" if platform.system() == "Windows" else "makejp2"))]}
 
-
 setup(
     **metadata,
     name=dcc_jp2_converter.__title__,
@@ -32,4 +34,4 @@ setup(
     version=dcc_jp2_converter.__version__,
     author=dcc_jp2_converter.__author__,
     author_email=dcc_jp2_converter.__author_email__,
-      )
+)
