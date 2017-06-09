7f172aeedc655c133239748d3802689d89adc90e
hborcher <hborcher@illinois.edu>
2017-06-09 14:38:16 -0500

    added FULL TITLE

--- dcc_jp2_converter/__init__.py
+++ dcc_jp2_converter/__init__.py
@@ -14,5 +14,5 @@ from .modules.kakaduDriver import KakaduCommandBuilder
 from .modules.kakaduDriver import kakaduCommandBuilders
 from .modules.converters import Converter
 from .utils import get_config_files
-from .__version__ import __version__, __author__, __author_email__, __url__, __title__, __description__
+from .__version__ import __version__, __author__, __author_email__, __url__, __title__, __description__, FULL_TITLE
 __all__ = ['modules']
