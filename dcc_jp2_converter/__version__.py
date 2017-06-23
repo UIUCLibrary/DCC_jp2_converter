25afbafe4fe114c78c162a564e1cc5260ca742eb
hborcher <hborcher@illinois.edu>
2017-06-23 15:55:38 -0500

    new version

--- dcc_jp2_converter/__version__.py
+++ dcc_jp2_converter/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'jp2Converter'
 __url__ = 'https://github.com/UIUCLibrary/DCC_jp2_converter'
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = 'DCC tool for building JP2 access files from Tiff files'
