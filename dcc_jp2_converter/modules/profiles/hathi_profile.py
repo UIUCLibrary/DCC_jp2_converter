5b94d8b7329bf1f1bbd866546fcef5f5c3c863ad
hborcher <hborcher@illinois.edu>
2017-09-14 11:32:50 -0500

    PSR-32 Added optional destination argument for convert_access_folder

--- dcc_jp2_converter/modules/profiles/hathi_profile.py
+++ dcc_jp2_converter/modules/profiles/hathi_profile.py
@@ -3,7 +3,7 @@ import os
 from dcc_jp2_converter.modules.profiles.profile import AbsProfile
 import logging
 from dcc_jp2_converter import Converter
-
+import typing
 
 class HathiProfile(AbsProfile):
     logger = logging.getLogger(__name__)
@@ -21,16 +21,18 @@ class HathiProfile(AbsProfile):
                 self.logger.debug("Configuring setting to remove original file on success.")
             self.remove_on_success = kwargs['remove_on_success']
 
-    def convert_access_folder(self, path):
+    def convert_access_folder(self, path, destination=None):
+        destination = destination or path
         self.logger.debug("Converting access folder of {} with the HathiProfile".format(path))
-        self.converter.convert_tiff_access_folder(
-            path=path,
+        self.converter.convert_tiff_access_folder2(
+            source=path,
+            destination=destination,
             overwrite_existing=self.overwrite,
             remove_on_success=self.remove_on_success
         )
 
     @staticmethod
-    def find_access_folders(path)->str:
+    def find_access_folders(path)->typing.Iterator[str]:
         """
         Searches the path recursively for a folder named "access" and yields every folder within that access folder.
 
